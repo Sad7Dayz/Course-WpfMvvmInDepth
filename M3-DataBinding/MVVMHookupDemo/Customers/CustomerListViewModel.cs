840ee1da3d23a9bffccfe187547553a5b535d970
Yves Gingras <yves.gingras@icloud.com>
2017-10-28 17:56:03 -0400

    Demo "M3-DataBinding" completed.

---
+++ M3-DataBinding/MVVMHookupDemo/Customers/CustomerListViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using MVVMHookupDemo.Services;
+using Zza.Data;
+
+namespace MVVMHookupDemo.Customers
+{
+    public class CustomerListViewModel
+    {
+        private ICustomersRepository _repository = new CustomersRepository();
+        private ObservableCollection<Customer> _customers;
+
+        public CustomerListViewModel()
+        {
+            if (DesignerProperties.GetIsInDesignMode(
+                new System.Windows.DependencyObject())) return;
+
+            Customers = new ObservableCollection<Customer>( _repository.GetCustomersAsync().Result);
+        }
+
+        public ObservableCollection<Customer> Customers {
+            get {
+                return _customers;
+            }
+            set {
+                _customers = value;
+            }
+        }
+    }
+}
