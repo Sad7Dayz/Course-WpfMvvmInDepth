38b66309a5d8fe8e7344cffbc53c0dbac059f3b0
Yves Gingras <yves.gingras@icloud.com>
2017-10-28 09:41:16 -0400

    Hands on completed

---
+++ M3-XAMLHookup/MVVMHookupDemo/Customers/CustomerListView.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Controls;
+
+namespace MVVMHookupDemo.Customers
+{
+    /// <inheritdoc cref="" />
+    /// <summary>
+    /// Logique d'interaction pour CustomerListView.xaml
+    /// </summary>
+    public partial class CustomerListView : UserControl
+    {
+        public CustomerListView()
+        {
+            /*Second way of statically hooking up the MVVM equation,
+             * instead of from the view (XAML)*/
+            this.DataContext = new CustomerListViewModel();
+            InitializeComponent();
+        }
+    }
+}
---
+++ M3-XAMLHookup/MVVMHookupDemo/Customers/CustomerListViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using MVVMHookupDemo.Services;
+using Zza.Data;
+
+namespace MVVMHookupDemo.Customers
+{
+    public class CustomerListViewModel
+    {
+        private ICustomersRepository _customersRepository = new CustomersRepository();
+        public ObservableCollection<Customer> Customers { get; set; }
+
+        public CustomerListViewModel() {
+
+
+            if (DesignerProperties.GetIsInDesignMode(new System.Windows.DependencyObject())) return; 
+                /*cannot use async and await keywords in a constructor.
+             * The 'Result' property on a task makes the method execute synchronously.
+             * And, we wrap it 'cause we need an Observable collection.            */
+                Customers = new ObservableCollection<Customer>(_customersRepository.GetCustomersAsync().Result);
+        }
+    }
+}
