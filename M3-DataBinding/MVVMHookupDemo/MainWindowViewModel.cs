3fddda24d9fc7ea71c8e18028853ed7ee07a955b
Yves Gingras <yves.gingras@icloud.com>
2017-10-29 13:55:22 -0400

    Completed demos of module 3.

---
+++ M3-DataBinding/MVVMHookupDemo/MainWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿using MVVMHookupDemo.Customers;
+
+namespace MVVMHookupDemo
+{
+    //Demo5- Implicit DataTemplate for "ViewModel First MVVM  hookup (in the  Main View associated files.)
+    public class MainWindowViewModel
+    {
+        public MainWindowViewModel() {
+            CurrentViewModel = new CustomerListViewModel();
+        }
+
+        public object CurrentViewModel { get; set; }
+    }
+}
