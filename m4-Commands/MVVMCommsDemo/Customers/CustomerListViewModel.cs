c64b6fdd54ca9211fb8e335d93530bdb966768e1
Yves Gingras <yves.gingras@icloud.com>
2017-11-05 17:18:29 -0500

    Module 4; Completed demo1, "Commands for View to ViewModel Communication.

--- m4-Commands/MVVMCommsDemo/Customers/CustomerListViewModel.cs
+++ m4-Commands/MVVMCommsDemo/Customers/CustomerListViewModel.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Input;
 using MVVMCommsDemo.Services;
 using Zza.Data;
 
@@ -12,28 +8,41 @@ namespace MVVMCommsDemo.Customers
 {
     public class CustomerListViewModel
     {
-        private ObservableCollection<Customer> _customers;
         private ICustomersRepository _repository = new CustomersRepository();
-        
+        private Customer _selectedCustomer;
+
+        //M4-Demo 'Commands for View to ViewModel communication'
+        public RelayCommand DeleteCommand { get; }
+
+        public ObservableCollection<Customer> Customers { get; set; }
+
+        public Customer SelectedCustomer {
+            get => _selectedCustomer;
+            set {
+                _selectedCustomer = value;
+                DeleteCommand.RaiseCanExecuteChanged();
+            }
+        }
+
         public CustomerListViewModel()
         {
             if (DesignerProperties.GetIsInDesignMode(
                 new System.Windows.DependencyObject())) return;
 
             Customers = new ObservableCollection<Customer>( _repository.GetCustomersAsync().Result);
+            DeleteCommand = new RelayCommand(OnDelete, CanDelete);
         }
 
-        public ObservableCollection<Customer> Customers
-        {
-            get
-            {
-                return _customers;
-            }
-            set
-            {
-                _customers = value;
-            }
+        private bool CanDelete() {
+            return SelectedCustomer != null;
         }
 
+        private void OnDelete() {
+            Customers.Remove(SelectedCustomer);
+
+        }
+
+       
     }
 }
+ 
