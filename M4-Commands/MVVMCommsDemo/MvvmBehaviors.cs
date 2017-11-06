8e17cae1f55cc33d8662a6a41666b86242b9cbc6
Yves Gingras <yves.gingras@icloud.com>
2017-11-06 17:43:34 -0500

    Module 4; Completed demo 5, "Custom Blend Behavior with INotifyPropertyChanged Trigger".

--- M4-Commands/MVVMCommsDemo/MvvmBehaviors.cs
+++ M4-Commands/MVVMCommsDemo/MvvmBehaviors.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 
-namespace MVVMCommsDemo.Customers
+namespace MVVMCommsDemo
 {
     public static class MvvmBehaviors
     {
--- m4-Commands/MVVMCommsDemo/MainWindowViewModel.cs
+++ m4-Commands/MVVMCommsDemo/MainWindowViewModel.cs
@@ -1,18 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
+//using System.Threading;
 using System.Threading.Tasks;
+using System.Timers;
 using MVVMCommsDemo.Customers;
 
 namespace MVVMCommsDemo
 {
-    public class MainWindowViewModel
+    public class MainWindowViewModel :INotifyPropertyChanged
     {
+        private string _notificationMessage;
+        private Timer _timer = new Timer(5000);
+
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+        public object CurrentViewModel { get; set; }
+
         public MainWindowViewModel()
         {
             CurrentViewModel = new CustomerListViewModel();
+            _timer.Elapsed += (s, e) =>
+                NotificationMessage = "At the tone the time will be: " + DateTime.Now.ToLocalTime();
+            _timer.Start();
         }
-        public object CurrentViewModel { get; set; }
+
+        public string NotificationMessage {
+            get => _notificationMessage;
+            set {
+                if (value == _notificationMessage) return;
+                _notificationMessage = value;
+                PropertyChanged(this,new PropertyChangedEventArgs("NotificationMessage"));
+            }
+        }
     }
 }
