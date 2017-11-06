f789ae1f79016d9e47f113c297bcd4cdc5d41d63
Yves Gingras <yves.gingras@icloud.com>
2017-11-06 11:14:06 -0500

    Module 4; Completed demo2, "Custom Attached Behavior".

--- m4-Commands/MVVMCommsDemo/Customers/CustomerListViewModel.cs
+++ m4-Commands/MVVMCommsDemo/Customers/CustomerListViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Threading.Tasks;
 using System.Windows.Input;
 using MVVMCommsDemo.Services;
 using Zza.Data;
@@ -24,13 +25,16 @@ namespace MVVMCommsDemo.Customers
             }
         }
 
-        public CustomerListViewModel()
-        {
+        public CustomerListViewModel() {
+            DeleteCommand = new RelayCommand(OnDelete, CanDelete);
+        }
+
+
+        public async void LoadCustomers() {
             if (DesignerProperties.GetIsInDesignMode(
                 new System.Windows.DependencyObject())) return;
-
-            Customers = new ObservableCollection<Customer>( _repository.GetCustomersAsync().Result);
-            DeleteCommand = new RelayCommand(OnDelete, CanDelete);
+             
+            Customers = new ObservableCollection<Customer>(await _repository.GetCustomersAsync());
         }
 
         private bool CanDelete() {
