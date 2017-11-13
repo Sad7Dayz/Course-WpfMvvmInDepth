5ed95fe2889cf4f2898dffa271c0ddbc38081dda
Yves Gingras <yves.gingras@icloud.com>
2017-11-13 17:21:40 -0500

    Module 5; Completed demo 2 "Building Out an MVVM Hierarchy".

---
+++ M5-ZzaDesktop/ZzaDesktop/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZzaDesktop.Customers;
+using ZzaDesktop.OrderPrep;
+using ZzaDesktop.Orders;
+
+namespace ZzaDesktop
+{
+    public class MainWindowViewModel
+    {
+        private CustomerListViewModel _customerListViewModel = new CustomerListViewModel();
+        private OrderViewModel _orderViewModel = new OrderViewModel();
+        private OrderPrepViewModel _orderPrepViewModel = new OrderPrepViewModel();
+
+        public object CurrentViewModel { get; set; }
+    }
+}
---
+++ M5-ZzaDesktop/ZzaDesktop/OrderPrep/OrderPrepView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace ZzaDesktop.OrderPrep
+{
+    /// <summary>
+    /// Logique d'interaction pour OrderPrepView.xaml
+    /// </summary>
+    public partial class OrderPrepView : UserControl
+    {
+        public OrderPrepView()
+        {
+            InitializeComponent();
+        }
+    }
+}
