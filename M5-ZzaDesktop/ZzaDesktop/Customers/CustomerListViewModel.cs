1260902e04a24e56b5f6e722afb51ac2230fd721
Yves Gingras <yves.gingras@icloud.com>
2017-11-19 17:25:53 -0500

    Module 5; Completed demo 5'Data Loading into a ViewModel'

--- M5-ZzaDesktop/ZzaDesktop/Customers/CustomerListViewModel.cs
+++ M5-ZzaDesktop/ZzaDesktop/Customers/CustomerListViewModel.cs
@@ -1,6 +1,37 @@
-﻿namespace ZzaDesktop.Customers
+﻿using System;
+using System.Collections.ObjectModel;
+using Zza.Data;
+using ZzaDesktop.Services;
+
+namespace ZzaDesktop.Customers
 {
     public class CustomerListViewModel :BindableBase
     {
+        private ICustomersRepository _repo = new CustomersRepository();
+        private ObservableCollection<Customer> _customers;
+
+
+        public ObservableCollection<Customer> Customers {
+            get { return _customers; }
+            set { SetProperty(ref _customers,value); }
+        }
+
+        public RelayCommand<Customer> PlaceOrderCommand { get; private set; }
+
+
+        public CustomerListViewModel() {
+            PlaceOrderCommand = new RelayCommand<Customer>(OnPlaceOrder);    
+        }
+
+        private void OnPlaceOrder(Customer customer) {
+
+        }
+
+        public async void LoadCustomers() {
+            Customers = new ObservableCollection<Customer>(await _repo.GetCustomersAsync());
+        }
+
     }
 }
+
+ 
