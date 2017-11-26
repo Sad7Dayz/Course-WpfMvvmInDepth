a38589505dbbbeb35e6e529b829baebbcd4a96ae
Yves Gingras <yves.gingras@icloud.com>
2017-11-26 16:56:34 -0500

    Module6; Demo1, 'Scaffolding an Edit View in Visual Studio'

--- M6-ZzaDesktop/ZzaDesktop/Customers/AddEditCustomerView.xaml.cs
+++ M6-ZzaDesktop/ZzaDesktop/Customers/AddEditCustomerView.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ZzaDesktop.Customers
 {
@@ -23,6 +11,23 @@ namespace ZzaDesktop.Customers
         public AddEditCustomerView()
         {
             InitializeComponent();
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void UserControl_Loaded_1(object sender, RoutedEventArgs e)
+        {
+
+            // Ne chargez pas vos données au moment du design.
+            // if (!System.ComponentModel.DesignerProperties.GetIsInDesignMode(this))
+            // {
+            // 	//Chargez vos données ici et assignez le résultat à CollectionViewSource.
+            // 	System.Windows.Data.CollectionViewSource myCollectionViewSource = (System.Windows.Data.CollectionViewSource)this.Resources["Resource Key for CollectionViewSource"];
+            // 	myCollectionViewSource.Source = your data
+            // }
         }
     }
 }
