09402b7db50973d60b78b3147636fd3d0375148e
Yves Gingras <yves.gingras@icloud.com>
2017-12-04 03:57:45 -0500

    Module 6; Demo 6 'Searching and Filtering Data'.

--- M6-ZzaDesktop/ZzaDesktop/Customers/CustomerListViewModel.cs
+++ M6-ZzaDesktop/ZzaDesktop/Customers/CustomerListViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using Zza.Data;
 using ZzaDesktop.Services;
 
@@ -7,31 +9,55 @@ namespace ZzaDesktop.Customers
 {
     public class CustomerListViewModel :BindableBase
     {
-        private ICustomersRepository _repo ;//= new CustomersRepository();
+        private ICustomersRepository _repo ;
+        private List<Customer> _allCustomers;
         private ObservableCollection<Customer> _customers;
-
-        // <summary>To handled by the parent ViewModel, the <see cref="MainWindowViewModel"/>.</summary>
-        public event Action<Guid> PlaceOrderRequested = delegate { };
-        public event Action<Customer> AddCustomerRequested = delegate { };
-         public event Action<Customer> EditCustomerRequested = delegate { };
-
-        //public event Action<Guid>  PlaceOrderRequested = delegate { };
-        //public event Action<Guid> PlaceOrderRequested = delegate { };
-        public ObservableCollection<Customer> Customers {
-            get { return _customers; }
-            set { SetProperty(ref _customers,value); }
-        }
-
-        public RelayCommand<Customer> PlaceOrderCommand { get;}
-        public RelayCommand AddCustomerCommand { get; }
-        public RelayCommand<Customer> EditCustomerCommand { get; }
-
+        private string _searchInput;
 
         public CustomerListViewModel(ICustomersRepository repo) {
             _repo = repo;
             PlaceOrderCommand = new RelayCommand<Customer>(OnPlaceOrder);
             AddCustomerCommand = new RelayCommand(OnAddCustomer);
-            EditCustomerCommand = new RelayCommand<Customer>(OnEditCustomer);    
+            EditCustomerCommand = new RelayCommand<Customer>(OnEditCustomer);
+            ClearSearchCommand = new RelayCommand(OnClearSearch);
+
+        }
+
+
+        public event Action<Guid> PlaceOrderRequested = delegate { };
+        public event Action<Customer> AddCustomerRequested = delegate { };
+        public event Action<Customer> EditCustomerRequested = delegate { };
+
+        public ObservableCollection<Customer> Customers {
+            get => _customers;
+            set => SetProperty(ref _customers,value);
+        }
+        public string SearchInput {
+            get => _searchInput;
+            set {
+                SetProperty(ref _searchInput, value);
+                FilterCustomers(_searchInput);
+            }
+        }
+        public RelayCommand<Customer> PlaceOrderCommand { get; }
+        public RelayCommand AddCustomerCommand { get; }
+        public RelayCommand<Customer> EditCustomerCommand { get; }
+        public RelayCommand ClearSearchCommand { get;}
+
+
+        /// <summary>Triggered by the 'Loaded' event of the <see cref="CustomerListView"/>.
+        /// It then populates the view the list of customers.</summary>
+        public async void LoadCustomers() {
+            _allCustomers = await _repo.GetCustomersAsync();
+            Customers = new ObservableCollection<Customer>(_allCustomers);
+        }
+
+        private void FilterCustomers(string searchInput) {
+            Customers = string.IsNullOrWhiteSpace(searchInput)
+                ? new ObservableCollection<Customer>(_allCustomers)
+                : new ObservableCollection<Customer>(_allCustomers
+                    .Where(c => c.FullName.ToLower()
+                        .Contains(searchInput.ToLower())));
         }
 
         private void OnEditCustomer(Customer cust) {
@@ -46,12 +72,9 @@ namespace ZzaDesktop.Customers
             PlaceOrderRequested(customer.Id);
         }
 
-        /// <summary>Triggered by the 'Loaded' event of the <see cref="CustomerListView"/>.
-        /// It then populates the view the list of customers.</summary>
-        public async void LoadCustomers() { 
-            Customers = new ObservableCollection<Customer>(await _repo.GetCustomersAsync());
+        private void OnClearSearch() {
+            SearchInput = null;
         }
-
     }
 }
 
